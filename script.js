b534978b5768cf6570a773fd70e45d88589df216
Lo-Deck <122597745+Lo-Deck@users.noreply.github.com>
2024-04-03 15:13:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const myForm = document.getElementById('myForm');
+
+const error = document.querySelectorAll('.messError');
+
+
+myForm.addEventListener('submit', function(event){
+
+    for(let i = 0; i < myForm.length-1; i++)
+    {
+
+        if(!myForm[i].validity.valid)
+        {
+
+            error[i].style.display = 'inline';
+            myForm[i].style.border = 'red 1px solid';
+            myForm[i].placeholder = '';
+
+            if(i === 2)
+            {
+                myForm[i].value = '';
+                myForm[i].style.color = 'red';
+                myForm[i].placeholder = 'email@example.com';
+            }
+
+        }
+
+        else
+        {
+            error[i].style.display = 'none';
+            myForm[i].style.color = '#000';
+            myForm[i].style.border = '1px solid hsla(0, 0%, 0%, 0.3)';   
+        }
+    
+    }
+
+    event.preventDefault();
+
+});
+
+
+
